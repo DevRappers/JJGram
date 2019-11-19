40d3870b9fe40ab7e7a19e4d4ba5151a622c7268
DevRappers <devrappers@gmail.com>
2019-11-19 09:46:38 +0900

    follow unfollow Resolver ( 팔로우 언팔로우 기능 개발)

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ InstaGram Clone Project
 - [x] Comment on a photo : 댓글 달기 
 - [x] Search by user : user검색
 - [x] Search by location : 위치로 검색 
+- [x] Follow User : 팔로우
+- [x] Unfollow User : 언팔로우
 - [ ] See user profile : 사용자의 프로필 구경 
-- [ ] Follow / Unfollow User : 팔로우, 언팔로우 
 - [ ] See the full photo : 사진을 크게 보기 
 - [ ] Edit my profile : 프로필 정보 수정 
 - [ ] Upload a photo : 사진 업로드
--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,6 +7,15 @@ export default {
             const { secret, email} = args;
             const user = await prisma.user({email});
             if(user.loginSecret === secret){
+                // 시크릿코드가 항상 달라야 하기 때문에 로그인 되면 secret코드를 비워준다.
+                await prisma.updateUser({
+                    where:{
+                        id: user.id
+                    },
+                    data:{
+                        loginSecret: ''
+                    }
+                });
                 // 만약 user.loginSecret가 secret와 같다면 JWT토큰을 return
                 return generateToken(user.id);
             }else{
