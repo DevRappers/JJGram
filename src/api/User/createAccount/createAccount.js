3a8faebe9d5c99b3a3587e3dc1a07c18835000cd
DevRappers <devrappers@gmail.com>
2019-11-22 10:52:31 +0900

    query me 부분 수정

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,9 +4,16 @@ export default {
 	Mutation: {
 		createAccount: async (_, args) => {
 			const { name, email, firstName = '', lastName = '', bio = '' } = args;
-			const exists = await prisma.$exists.user({ name });
+			const exists = await prisma.$exists.user({
+				OR: [
+					{
+						username
+					},
+					{ email }
+				]
+			});
 			if (exists) {
-				throw Error('이미 존재하는 이름입니다.');
+				throw Error('이미 존재하는 이름/이메일 입니다.');
 			}
 			await prisma.createUser({
 				name,
