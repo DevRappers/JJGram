e1510e0fc3e2545877b858fae4331f0b948f014e
DevRappers <devrappers@gmail.com>
2019-11-24 15:10:38 +0900

    server문재 해결 및 영어 -> 한글로 언어변경

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 // dotenv을 불러옴
-import "./env";
-import { GraphQLServer } from "graphql-yoga";
-import logger from "morgan";
-import schema from "./schema";
-import "./passport";
-import { authenticateJwt } from "./passport";
-import { isAuthenticated } from "./middlewares";
+import './env';
+import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
+import './passport';
+import { authenticateJwt } from './passport';
+import { isAuthenticated } from './middlewares';
 
 // env에 PORT가 지정되어 있지 않다면 4000번으로 설정
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
-     schema, 
-     context: ({request}) => ({request, isAuthenticated})
+	schema,
+	context: ({ request }) => ({ request, isAuthenticated })
 });
 
 // graphqlyoga에 express가 있어 express서버에 접근 가능함.
-server.express.use(logger("dev"));
+server.express.use(logger('dev'));
 server.express.use(authenticateJwt);
 
 server.start({ port: PORT }, () => console.log(`Server running on http://localhost:${PORT}`));
