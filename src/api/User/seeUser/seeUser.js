4dad037544f0d8d48f36226cccff206df53d388c
DevRappers <devrappers@gmail.com>
2019-11-26 10:20:25 +0900

    profile관련 수정 postCount등

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -1,10 +1,10 @@
-import { prisma } from "../../../../generated/prisma-client";
+import { prisma } from '../../../../generated/prisma-client';
 
 export default {
-    Query:{
-        seeUser: async(_, args) => {
-            const { id } = args;
-            return prisma.user({ id });
-        }
-    }
+	Query: {
+		seeUser: async (_, args) => {
+			const { name } = args;
+			return prisma.user({ name });
+		}
+	}
 };
